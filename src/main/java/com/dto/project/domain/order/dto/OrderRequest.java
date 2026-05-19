08ce7fe7737efa43ba1ff774addf09cfe44fefa5
kkomakk <j2031414@gmail.com>
2026-05-19 16:23:34 +0900

    feature/order-refactoring

--- src/main/java/com/dto/project/domain/order/dto/OrderRequest.java
+++ src/main/java/com/dto/project/domain/order/dto/OrderRequest.java
@@ -1,5 +1,23 @@
 package com.dto.project.domain.order.dto;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderRequest {
 
+    private List<OrderItemDto> orderItems;
+    private String deliveryAddress; // 배송지 주소
+    private String requestMessage;  // 배송 요청사항
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class OrderItemDto {
+        private Long productId;
+        private int quantity;
+    }
 }
--- src/main/java/com/dto/project/domain/order/dto/OrderResponse.java
+++ src/main/java/com/dto/project/domain/order/dto/OrderResponse.java
@@ -1,5 +1,21 @@
 package com.dto.project.domain.order.dto;
 
-public class OrderResponse {
+import com.dto.project.domain.order.entity.Order;
+import lombok.Getter;
 
+import java.time.LocalDateTime;
+
+@Getter
+public class OrderResponse {
+    private Long orderId;
+    private String orderStatus;     // ORDERED, CANCELED, SHIPPED
+    private int totalPrice;         // 총 결제 금액
+    private LocalDateTime orderDate; // 주문 일시
+
+    public OrderResponse(Order order) {
+        this.orderId = order.getId();
+        this.orderStatus = order.getStatus().name();
+        this.totalPrice = order.getTotalAmount();
+        this.orderDate = order.getCreatedAt();
+    }
 }
