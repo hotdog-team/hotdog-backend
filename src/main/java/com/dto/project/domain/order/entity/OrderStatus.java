1117d004cd9034762c61b3a4c16bb2f56a034de8
jeong in hyeok <dlsps252@naver.com>
2026-06-12 00:27:10 +0900

    feat: 반품 신청 기능 구현

--- src/main/java/com/dto/project/domain/order/entity/OrderStatus.java
+++ src/main/java/com/dto/project/domain/order/entity/OrderStatus.java
@@ -2,13 +2,14 @@ package com.dto.project.domain.order.entity;
 
 public enum OrderStatus {
 
-    PENDING,          	// 결제대기
-    PROCESSING,       	// 결제중
-    COMPLETED,        	// 결제완료
-    BEFORE_SHIPMENT,  	// 배송전
-    IN_TRANSIT,      	// 배송중
-    DELIVERED,        	// 배송완료
-    PARTIAL_CANCELLED,  // 부분취소
-    CANCELLED           // 취소완료
-
+    PENDING,              // 결제대기
+    PROCESSING,           // 결제중
+    COMPLETED,            // 결제완료
+    BEFORE_SHIPMENT,      // 배송준비
+    IN_TRANSIT,           // 배송중
+    DELIVERED,            // 배송완료
+    PARTIAL_CANCELLED,    // 부분취소
+    CANCELLED,            // 취소완료
+    RETURN_REQUESTED,     // 반품신청
+    RETURN_COMPLETED      // 반품완료
 }
